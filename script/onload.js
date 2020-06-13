e41cd78031a6d71c68d356cb1266ad5fd52cbd5a
sea <1284700369@qq.com>
2020-06-13 14:08:42 +0800

    update

--- script/onload.js
+++ script/onload.js
@@ -8,4 +8,18 @@ window.onload = () => {
     bd()
 
     xfc()
+
+    // 判断是否按下F12，F12键码为123  
+    window.onkeydown = window.onkeyup = window.onkeypress = function (event) {
+        if (event.keyCode = 123) {
+            event.preventDefault(); // 阻止默认事件行为  
+            window.event.returnValue = false;
+        }
+    }
+    console.log('no body!')
+
+    // 右键自定义事件
+    window.oncontextmenu = function () {
+        return false;
+    }
 }
